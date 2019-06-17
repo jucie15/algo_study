7c2358df20eccfb030d351d61b4cae093fc9246c
gustos <jucie15@nate.com>
2019-06-17 17:24:06 +0900

    프로그래머스 2018 윈터코딩 - 스킬트리 문제 추가

--- README.md
+++ README.md
@@ -25,8 +25,9 @@
 
   1. https://programmers.co.kr/learn/courses/30/lessons/42584?language=python3
   2. https://programmers.co.kr/learn/courses/30/lessons/42586
+  3. https://programmers.co.kr/learn/courses/30/lessons/49993
 
 - #### Leetcode
 
-- 
+-
 
---
+++ programmers/3-49993-스킬트리/gustos.py
@@ -0,0 +1,30 @@
+def solution(skill, skill_trees):
+    tmp_idx = -1
+    flag = True
+    answer = 0
+    cnt = 0
+
+    for st in skill_trees:
+        flag = True
+        tmp_idx = -1
+        for s in skill:
+            if s in st:
+                i = st.index(s)
+                if tmp_idx == -1:
+                    tmp_idx = i
+                elif tmp_idx > i:
+                    print('false', st)
+                    flag = False
+                    break
+                else:
+                    tmp_idx = i
+            else:
+                i = skill.index(s)
+                if i == 0:
+                    flag = False
+                    break
+        if flag:
+            print(st)
+            answer += 1
+
+    return answer
