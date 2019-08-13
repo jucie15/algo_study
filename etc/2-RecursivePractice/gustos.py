ae3643013258717e37799725efbae7b3894b029b
gustos <jucie15@nate.com>
2019-08-13 23:02:55 +0900

    재귀연습 추가

--- etc/2-RecursivePractice/gustos.py
+++ etc/2-RecursivePractice/gustos.py
@@ -4,5 +4,23 @@ def factorial(n):
     else:
         return n + factorial(n-1)
 
+def divide_digit(n):
+    if n < 10:
+        print(int(n))
+    else:
+        divide_digit(n / 10)
+        print(int(n % 10))
+
+def fibonacci(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
+
 if __name__ == "__main__":
-    factorial(10)
+    n = int(input("숫자를 입력해주세요: "))
+    # print(factorial(n))
+    divide_digit(n)
+    print(fibonacci(n))
