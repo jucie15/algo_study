37bf2df907f2800f578f29c95fbebbb225abad36
gustos <jucie15@nate.com>
2019-08-13 21:34:48 +0900

    수정렬하기 정답 추가

--- baekjoon/2750-수정렬하기/gustos.py
+++ baekjoon/2750-수정렬하기/gustos.py
@@ -1,20 +1,18 @@
 n = int(input())
 res = list()
 
-if n > 0:
-    for i in range(n):
-        num = int(input())
-        if i is 0:
+for i in range(n):
+    num = int(input())
+    if i == 0:
+        res.append(num)
+        continue
+    for j in range(0, len(res)):
+        if num < res[j]:
+            res.insert(j, num)
+            break
+        elif j + 1 == len(res):
             res.append(num)
-            continue
-        for j in range(0, len(res)):
-            if num < res[j]:
-                res.insert(j, num)
-                break
-            elif j + 1 is len(res):
-                res.append(num)
 
-    for i in range(len(res)-1):
-        print(res[i])
+for i in range(len(res)):
+    print(res[i])
 
-    print(res[n - 1])
